2fa041c218ffcfb23a03792ed553555121ca2b0c
Robert Pająk <pellared@hotmail.com>
2024-04-24 09:17:21 +0200

    Fix computation shutdown

--- signalflow/computation.go
+++ signalflow/computation.go
@@ -339,6 +339,7 @@ func (c *Computation) shutdown() {
 	close(c.dataChBuffer)
 	close(c.expirationChBuffer)
 	close(c.infoChBuffer)
+	close(c.eventChBuffer)
 }
 
 var ErrMetadataTimeout = errors.New("metadata value did not come in time")
